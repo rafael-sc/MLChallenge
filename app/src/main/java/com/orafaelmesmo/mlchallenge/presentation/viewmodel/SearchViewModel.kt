0eae9461e52ef1348e5788cc1e1e79846fe2d30f
Rafael Costa <rafael.costa2@ab-inbev.com>
2025-02-22 19:23:53 -0300

    added Scaffold and removed viewmodel from MainActivity

--- app/src/main/java/com/orafaelmesmo/mlchallenge/presentation/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/orafaelmesmo/mlchallenge/presentation/viewmodel/SearchViewModel.kt
@@ -6,38 +6,45 @@ import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.orafaelmesmo.mlchallenge.domain.model.Product
 import com.orafaelmesmo.mlchallenge.domain.usecase.SearchProductsUseCase
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.stateIn
 
 class SearchViewModel(
     private val searchProductsUseCase: SearchProductsUseCase,
 ) : ViewModel() {
+
     private val _searchState = MutableStateFlow<SearchState>(SearchState.Idle)
     val searchState: StateFlow<SearchState> get() = _searchState
 
     private val _searchQuery = MutableStateFlow("")
-    val searchQuery: StateFlow<String> get() = _searchQuery
+    private var lastQuery: String = ""
 
     @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
-    val productsPagingData: Flow<PagingData<Product>> = _searchQuery
+    val productsPagingData: StateFlow<PagingData<Product>> = _searchQuery
         .debounce(500)
         .distinctUntilChanged()
         .flatMapLatest { currentQuery ->
-            if (currentQuery.isNotEmpty()) {
+            if (currentQuery.isNotEmpty() && lastQuery != currentQuery) {
+                lastQuery = currentQuery
                 searchProductsUseCase.searchProducts(currentQuery)
                     .cachedIn(viewModelScope)
             } else {
                 flowOf(PagingData.empty())
             }
         }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = PagingData.empty()
+        )
 
     fun searchProducts(query: String) {
         _searchQuery.value = query
--- app/src/main/java/com/orafaelmesmo/mlchallenge/presentation/viewmodel/ui/ProductListScreen.kt
+++ app/src/main/java/com/orafaelmesmo/mlchallenge/presentation/viewmodel/ui/ProductListScreen.kt
@@ -1,34 +1,84 @@
 package com.orafaelmesmo.mlchallenge.presentation.viewmodel.ui
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.orafaelmesmo.mlchallenge.presentation.viewmodel.SearchViewModel
+import com.orafaelmesmo.mlchallenge.ui.components.AppTopBar
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductListScreen(
-    modifier: Modifier = Modifier
-) {
-    val viewModel: SearchViewModel = koinViewModel()
-    val productsPagingItems = viewModel.productsPagingData.collectAsLazyPagingItems()
-    viewModel.searchProducts("bola")
-    LazyColumn(
-        modifier = modifier.fillMaxSize()
+    modifier: Modifier = Modifier,
+
     ) {
-        items(productsPagingItems.itemCount) {
-            Text( //Todo change this to a ProductItem
-                text = productsPagingItems[it]?.name ?: "Name not found",
+    val viewModel: SearchViewModel = koinViewModel()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+        state = rememberTopAppBarState()
+    )
+    Scaffold(
+        modifier = modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            AppTopBar(
+                scrollBehavior = scrollBehavior,
+            ) {
+                viewModel.searchProducts(it)
+            }
+        },
+    ) { paddingValues ->
+        SearchContent(
+            paddingValues = paddingValues,
+            viewModel = viewModel
+        )
+    }
+}
+
+@Composable
+fun SearchContent(
+    paddingValues: PaddingValues,
+    viewModel: SearchViewModel
+) {
+
+
+    val lazyListState = rememberLazyListState()
+
+    val productsPagingItems = viewModel.productsPagingData.collectAsLazyPagingItems()
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        contentPadding = PaddingValues(
+            top = paddingValues.calculateTopPadding() + 16.dp,
+        ),
+        state = lazyListState
+    ) {
+        items(
+            count = productsPagingItems.itemCount,
+            key = { index ->
+                productsPagingItems[index]?.id ?: index
+            }
+        ) { index ->
+            Text(
+                text = productsPagingItems[index]?.name ?: "Name not found",
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp)
@@ -61,7 +111,7 @@ fun ProductListScreen(
 
                 loadState.refresh is LoadState.Error -> {
                     val e = loadState.refresh as LoadState.Error
-                    item { //Todo change this to a error item
+                    item {
                         Text(
                             text = "Error Loading Product: ${e.error.localizedMessage}",
                             modifier = Modifier
@@ -70,10 +120,9 @@ fun ProductListScreen(
                         )
                     }
                 }
-
                 loadState.append is LoadState.Error -> {
                     val e = loadState.append as LoadState.Error
-                    item { //Todo change this to a error item
+                    item {
                         Text(
                             text = "Erro ao carregar mais produtos: ${e.error.localizedMessage}",
                             modifier = Modifier
