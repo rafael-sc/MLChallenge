8e234e233d21f09ca7d397726d5dc8105ffd76e5
Rafael Costa <rafael.costa2@ab-inbev.com>
2025-02-22 21:38:37 -0300

    update domain Product object to receive only non nullable data

--- app/src/main/java/com/orafaelmesmo/mlchallenge/domain/model/Product.kt
+++ app/src/main/java/com/orafaelmesmo/mlchallenge/domain/model/Product.kt
@@ -2,7 +2,7 @@ package com.orafaelmesmo.mlchallenge.domain.model
 
 data class Product(
     val id: String,
-    val name: String? = "",
-    val price: Double? = 0.0,
-    val thumbnail: String? = "",
+    val name: String,
+    val price: String,
+    val thumbnail: String,
 )
