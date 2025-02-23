3e86955b79e017fd275ef84d6177f379157766bb
Rafael Costa <rafael.costa2@ab-inbev.com>
2025-02-22 21:37:25 -0300

    update AppToolbar layout

--- app/src/main/java/com/orafaelmesmo/mlchallenge/ui/components/AppTopBar.kt
+++ app/src/main/java/com/orafaelmesmo/mlchallenge/ui/components/AppTopBar.kt
@@ -1,13 +1,21 @@
 package com.orafaelmesmo.mlchallenge.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -16,12 +24,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Suppress("FunctionName")
 @Composable
 fun AppTopBar(
     modifier: Modifier = Modifier,
@@ -32,26 +41,64 @@ fun AppTopBar(
 
     TopAppBar(
         modifier =
-            modifier
-                .padding(horizontal = 16.dp)
-                .clip(RoundedCornerShape(100.dp)),
+        modifier
+            .padding(horizontal = 16.dp)
+            .clip(RoundedCornerShape(100.dp)),
         scrollBehavior = scrollBehavior,
         colors =
-            TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(),
-            ),
+        TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.9f), // Fundo semi-transparente
+        ),
         windowInsets = WindowInsets(top = 0.dp),
         title = {
-            TextField(
-                maxLines = 1,
-                value = textFieldValue,
-                onValueChange = { newValue ->
-                    textFieldValue = newValue
-                    onTextFieldValueChange(newValue)
-                },
-                label = { Text("Search product") },
+            Row(
                 modifier = Modifier.fillMaxWidth(),
-            )
-        },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search",
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+
+                TextField(
+                    value = textFieldValue,
+                    onValueChange = { newValue ->
+                        textFieldValue = newValue
+                        onTextFieldValueChange(newValue)
+                    },
+                    label = { Text("Search product") },
+                    singleLine = true,
+                    colors =
+                    TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent
+                    ),
+                    modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    shape = RoundedCornerShape(100.dp),
+                    trailingIcon = {
+                        if (textFieldValue.isNotEmpty()) {
+                            IconButton(
+                                onClick = { textFieldValue = "" }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Close,
+                                    contentDescription = "Clear",
+                                    tint = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            }
+                        }
+                    }
+                )
+            }
+        }
     )
 }
