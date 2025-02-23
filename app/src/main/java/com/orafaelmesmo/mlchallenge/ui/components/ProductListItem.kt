727b1f651912f17c9fea625696d5143be5467037
Rafael Costa <rafael.costa2@ab-inbev.com>
2025-02-22 21:36:58 -0300

    add ProductListItem.kt

---
+++ app/src/main/java/com/orafaelmesmo/mlchallenge/ui/components/ProductListItem.kt
@@ -0,0 +1,57 @@
+package com.orafaelmesmo.mlchallenge.ui.components
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.network.NetworkHeaders
+import coil3.network.httpHeaders
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+
+
+@Composable
+fun ProductListItem(
+    productName: String,
+    productValue: String,
+    imageUrl: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        val model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .httpHeaders(NetworkHeaders.Builder().add("User-Agent", "Mozilla/5.0").build())
+            .crossfade(true)
+            .build()
+        AsyncImage(
+            model = model,
+            contentDescription = "Imagem do produto: $productName",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(80.dp)
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column {
+            Text(
+                text = productName,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                modifier = Modifier.padding(top = 4.dp),
+                text = "R\$ $productValue",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
