27d06aaf7f56d0e9459b9a4285275b44180efdd1
Rafael Costa <rafael.costa.ti@gmail.com>
2025-02-25 17:17:58 -0300

    add NetworkUtils an ResourceProvider

---
+++ app/src/main/java/com/orafaelmesmo/mlchallenge/commom/ResourceProvider.kt
@@ -0,0 +1,24 @@
+package com.orafaelmesmo.mlchallenge.commom
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+
+class ResourceProvider(private val context: Context) {
+
+    fun getString(resId: Int, vararg formatArgs: Any): String {
+        return context.getString(resId, *formatArgs)
+    }
+
+    fun getColor(resId: Int): Int {
+        return ContextCompat.getColor(context, resId)
+    }
+
+    fun getDrawable(resId: Int): Drawable? {
+        return ContextCompat.getDrawable(context, resId)
+    }
+
+    fun getDimension(resId: Int): Float {
+        return context.resources.getDimension(resId)
+    }
+}
